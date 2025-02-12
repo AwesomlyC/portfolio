5d9be0e204235c0319fb0f1a3bff5c27eece697d
Victor Chhun <109203183+AwesomlyC@users.noreply.github.com>
2025-02-12 12:29:39 -0800

    added react-type-animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import Hero from '@/components/Hero';
+import MobileNavbar from '@/components/MobileNavbar';
+import Navbar from '@/components/Navbar';
+import React, {useState} from 'react'
 
 const HomePage = () => {
+
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
+
   return (
-    <div className='text-9xl text-red-900'>HomePage</div>
+    <div className='overflow-x-hidden'>
+      <div>
+        {/* Navbar section web/mobile */}
+        <MobileNavbar nav={nav} closeNav={closeNav}/>
+        <Navbar openNav = {openNav}/>
+        {/* Hero Section */}
+        <Hero />
+      </div>
+     
+      
+      </div>
   )
 }
 
