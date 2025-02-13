8907d163149d2ca8fd6df5609962e99d1f2a918c
Victor Chhun <109203183+AwesomlyC@users.noreply.github.com>
2025-02-12 21:34:58 -0800

    updated with projects.

---
+++ components/ProjectItem.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+
+interface Props {
+    title: string,
+    description: string,
+    skills: Array<string>,
+    githublink: string,
+    websitelink: string
+}
+
+
+const ProjectItem = ({title, description, skills, githublink, websitelink} : Props) => {
+  return (
+    <div
+      className="group w-full transform duration-300 rounded-2xl border border-slate-500/20 
+    dark:border-slate-600/30 bg-slate-100/20 dark:bg-slate-600/20 hover:bg-slate-100/30 
+    dark:hover:bg-slate-600/30 cursor-pointer p-4 sm:p-6 flex flex-col justify-between"
+    >
+      <header>
+        <h3 className="pb-4 font-semibold text-lg text-white">{title}</h3>
+        <p className="text-white opacity-80">
+            {description}
+        </p>
+      </header>
+      <footer>
+        <div className="mb-6 flex flex-wrap">
+          {skills.map( (skill) => (
+          <div className='project-skill' key = {skill}>{skill} </div>))
+          }
+        </div>
+        <ul className="flex space-x-3 text-[18px]">
+{          githublink ? <li className="z-10">
+            <a href={githublink}>
+              <FaGithub className="project-icon" />
+            </a>
+          </li> : null}
+{          websitelink ? <li className="z-10">
+            <a href={websitelink}>
+              <FaExternalLinkAlt className="project-icon" />
+            </a>
+          </li> : null}
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default ProjectItem;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,22 +1,76 @@
 // import Image from 'next/image'
-import React from 'react'
+import React from "react";
+import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
-    <div className='bg-[#02050a] pt-[4rem] md:pt-[8rem] pb-[1rem]'>
-        <h1 className='heading'>
-            Pro<span className='text-yellow-400'>ject</span>
-        </h1>
-        <div className='w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem] '>
-            <div>
-                <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
- 
-                </div>
-            </div>
-
+    <div className="bg-[#02050a] pt-[4rem] md:pt-[8rem] pb-[1rem] h-[100%]">
+      <h1 className="heading">
+        Pro<span className="text-yellow-400">jects</span>
+      </h1>
+      <div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem] ">
+        {/* <div className='w-[80%] pt-[2rem] mx-auto grid grid-cols-2 gap-[2rem]'> */}
+        <div className="project-item">
+          <ProjectItem
+            title="Job Tech Stack"
+            description={`Full-stack application that enables users to input job details, 
+              extract and analyze technical keywords (Java, SQL, and React) from job 
+              descriptions. It visualizes this data using pie and line charts, 
+              allowing users to track job application trends and gain insights over time`}
+            skills={["React", "Javascript", "Express.js", "Node.js", 
+              "MongoDB", "Chart.js", "Axios", "Vercel", "Clerk"]}
+            githublink="https://github.com/AwesomlyC/JobTechStack"
+            websitelink="https://job-tech-stack-frontend.vercel.app"
+          />
         </div>
-    </div>
-  )
-}
+        <div className="project-item">
+          <ProjectItem
+            title="To-Do List Application"
+            description="Robust to-do list application to allow users to create a list without 
+            creating an account. Capable of CRUD operations utilizing Spring Boot to perform such actions"
+            skills={["Java", "Javascript", "React", "Node.js", "MongoDB", "Spring Boot"]}
+            githublink="https://github.com/AwesomlyC/To_Do_Application"
+            websitelink=""
+          />
+        </div>
+        <div className="project-item">
+          <ProjectItem
+            title="Fabflix Movie Database Web Application"
+            description="Fully build architecture from end-to-end that is capable of performing all features
+            under 500 milliseconds. Coordinated with a colleague to deploy this application on AWS EC2. Application that displays
+            a catalog of 1000s of movies and allow users to 'checkout' those movies."
+            skills={["Java", "Javascript", "AWS", 'jQuery']}
+            githublink=""
+            websitelink=""
+          />
+        </div>
+        <div className="project-item">
+          <ProjectItem
+            title="Ranked Search Engine"
+            description="Search Engine capable of parsing a corpus comprising 56,000 documents from UCI ICS servers.
+            Integrated an inverted index matrix for efficient document retrieval and arranged a TF-IDF scoring mechanism for
+            document relevancy. Additionally, deployed the application with Flask capable of displaying top search results in 
+            under 300 milliseconds."
+            skills={["Python", "Flask"]}
+            githublink="https://github.com/AwesomlyC/Ranked-Search-Engine"
+            websitelink=""
+          />
+        </div>
 
-export default Projects
+        <div className="project-item">
+          <ProjectItem
+            title="Petr Run"
+            description={`Hackathon 2023 project at Hack@UCI and won Best Beginner's Hack. Fully functional video game to showcase the 
+              culture of UCI for incoming undergraduates. Engineered the backend infrastructure to enable scalability 
+              across multiple scenes and support additional features.`}
+            skills={["Godot", "GDScript"]}
+            githublink="https://devpost.com/software/petr-run"
+            websitelink=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
