c5728c65856460e27d955fb9ac9c96c7bd6928fb
Victor Chhun <109203183+AwesomlyC@users.noreply.github.com>
2025-02-13 13:08:14 -0800

    added more titles to texteffect.

--- components/TextEffect.tsx
+++ components/TextEffect.tsx
@@ -7,9 +7,13 @@ const TextEffect = () => {
         // Same substring at the start will only be typed out once, initially
         'Software Engineer',
         1500, // wait 1s before replacing "Mice" with "Hamsters"
+        'Fullstack Developer',
+        1500,
+        'Backend Developer',
+        1500,
         'Web Developer',
         1500,
-        'Code',
+        'Coder',
         1500,
       ]}
       speed={50}
