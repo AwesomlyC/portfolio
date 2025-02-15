2972a03e7677a1c33fd77b6f219b1c272933a4d2
Victor Chhun <109203183+AwesomlyC@users.noreply.github.com>
2025-02-14 16:33:35 -0800

    modified about page to more personal detailed.

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import MobileNavbar from "@/components/MobileNavbar";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Skills from "@/components/Skills";
-
+import SkillsList from "@/components/SkillsList";
 const About = () => {
   const [nav, setNav] = useState(false);
   const openNav = () => setNav(true);
@@ -12,7 +11,7 @@ const About = () => {
     <div>
       <MobileNavbar nav={nav} closeNav={closeNav} />
       <Navbar openNav={openNav} />
-      <div className="pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a] text-white">
+      <div className="pt-[2rem] md:pt-[4rem] pb-[5rem] bg-[#09101a] text-white">
         <h1 className="heading">
           ABOUT <span className="text-yellow-400">ME</span>
         </h1>
@@ -22,20 +21,69 @@ const About = () => {
         >
           <div className="space-y-6 text-left max-w-2xl">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus
-              tempora nihil aliquid quis, eos perspiciatis numquam libero ad sit
-              ea voluptas, nisi alias, magnam rem accusantium incidunt maxime
-              assumenda temporibus.
+              I graduated with a <text className='text-semibold text-blue-600 shadow-lg'>Bachelor&apos;s degree</text> in Computer Science from the University of California, Irvine, 
+              in June 2024, achieving a <text className='text-semibold text-blue-600 shadow-lg'>cumulative GPA</text> of <text className='text-semibold text-blue-600 shadow-lg'>3.698</text>. Throughout my academic journey, 
+              I developed a strong foundation in key areas such as Data Structures & Algorithms, 
+              Database Management Systems, and more. I have had the opportunity to showcase my skills 
+              by building a variety of <text className='text-semibold text-blue-600 shadow-lg'>projects</text>, including Job Tech Stack, Fabflix, and Petr Run, which have 
+              allowed me to apply my learning in real-world scenarios.
             </p>
+
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              nisi, temporibus animi, rem quaerat necessitatibus modi
-              reprehenderit, quos velit deserunt assumenda sapiente. Quibusdam
-              aspernatur eius omnis veniam tempora laboriosam ea.
+              Since <text className='text-semibold text-blue-600 shadow-lg'>graduation</text>, I have continued to expand my expertise by exploring new technologies and frameworks.
+              Over the past few months, I&apos;ve focused on developing small-scale applications using tools like 
+              React, Node.js, Express.js, MongoDB, and Django, among others. I am <text className='text-semibold text-blue-600 shadow-lg'>passionate</text> about learning and
+              experimenting with new technologies, and I thrive on discovering the endless possibilities for creating 
+              innovative solutions.
             </p>
+
+            <p>
+              What excites me most about <text className='text-semibold text-blue-600 shadow-lg'>Computer Science</text> is the opportunity to solve complex problems
+              and build impactful solutions. I&apos;m particularly interested in how technology can be used to address real-world 
+              challenges, whether it&apos;s through improving user interfaces, streamlining back-end systems, or optimizing performance. {''}
+              <text className='text-semibold text-blue-600 shadow-lg'>However</text>, what truly drives my passion for software development is its ability to solve even the most 
+              basic challenges that indivuals face in their <text className='text-semibold text-blue-600 shadow-lg'>day-to-day</text> lives. Software has the power to simplify tasks 
+              and enhance lives, and that&apos;s a <text className='text-semibold text-blue-600 shadow-lg'>key motivator</text> for me.
+            </p>
+
+            <p>
+              In addition, the <text className='text-semibold text-blue-600 shadow-lg'>collaborative</text> nature of software development -- working with others, exchanging ideas, 
+              and building solutions together -- is a trait I <text className='text-semibold text-blue-600 shadow-lg'>deeply value</text>. It&apos;s this dynamic that pushes the boundaries
+              of what&apos;s possible and continues to drive the evolution of the field.
+            </p>
+
           </div>
         </section>
-        <Skills />
+
+        <section className='flex flex-col justify-center items-center
+          pt-[2rem] pb-[2rem]  w-[100%] '>
+            <h1 className='heading'>Ski<span className='text-yellow-400'>lls</span></h1>
+           <div className='space-y-12 max-w-2xl'>
+            <SkillsList
+              skillTitleFirst="Lanaguages &"
+              skillTitleSecond="Databases"
+              skills={["Java", "Python", "JavaScript", "TypeScript", "MySQL", "PostgreSQL", 
+                "MongoDB", "NoSQL", "SQLite3", "Cassandra", "Couchbase"]}
+            />
+            <SkillsList
+              skillTitleFirst="Web"
+              skillTitleSecond="Development"
+              skills={["React", "Express.js", "Node.js", "Next.js", "JDBC", "jQuery", 
+                "Axios", "Django", "Tailwind CSS"]}
+            />
+            <SkillsList
+              skillTitleFirst="DevOps &"
+              skillTitleSecond="Tools"
+              skills={["Git", "GitHub", "Maven", "AWS", "Tomcat", "JMeter", "JUnit", "Postman", "Selenium"]}
+            />
+            <SkillsList
+              skillTitleFirst="APIs &"
+              skillTitleSecond="Architecture"
+              skills={["RESTFul APIs", "Microservices", "Clerk", "Vercel", "Netlify"]}
+            />
+          </div>
+        </section>
+
         <div>
           <h1 className="heading">
             CONTACT <span className="text-yellow-400">ME</span>
