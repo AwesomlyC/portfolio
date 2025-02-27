b42f1afc2a8efbdf0cdb74c0558fc12b32572797
Victor Chhun <109203183+AwesomlyC@users.noreply.github.com>
2025-02-27 15:17:01 -0800

    pushed hero content to z-100 to allow download of CV.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
         <Particle />
       </div>
       <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
-        <div className="h-[50%] w-[100%]">
+        <div className="h-[50%] w-[100%] z-[100]">
           <h1 className="text-[35px] md:text-[50px] text-white font-bold">
             HELLO, I&apos;M <span className="text-yellow-400">VICTOR!</span>
           </h1>
@@ -34,7 +34,9 @@ const Hero = () => {
           <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
             <button
               onClick={handleDownload}
-              className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items:center space-x-2"
+              className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] 
+              text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items:center 
+              space-x-2 cursor-pointer"
             >
               <p>Download CV</p>
               <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
